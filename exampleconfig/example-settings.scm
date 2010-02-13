f146c06fae9330f093eac7cd68000e27eaad9a09
Christian Jaeger <christian.jaeger@umontreal.ca>
2010-02-13 12:54:12 -0500

    Better explanation of boolean / maybe handling

--- exampleconfig/example-settings.scm
+++ exampleconfig/example-settings.scm
@@ -1,8 +1,15 @@
 ;; ---- settings
 
 ;; This is Scheme code;
-;; booleans are #f for false and #t for true
-;;  (actually all non-#f values are interpreted as true)
+
+;; Booleans are #f for false and #t for true. Actually all non-#f
+;; values are interpreted as true: so there are two situations where
+;; #f is used, for one in places where only false and true is
+;; distinguished (Boolean data type), and in which case you'd say #t
+;; to say true, and in places where optionally there can be given some
+;; value and the actual non-false value is being relevant (Maybe data
+;; type in some other languages). By convention, variables that only
+;; take booleans are ending with a question mark.
 
 ;; You can create definitions on your own, too (better start your own
 ;; variables with an underscore to avoid potential conflicts), and use
